a0766f5ea30236c791ca75a935649cbc59305fb2
Cora Krynick <krynickcora@gmail.com>
2020-06-19 12:21:07 -0500

    finished mvp

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,14 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+//nestedFunction is the child of myFunction. Closures allows nestedFunction access to `const internal` from the parent `myFunction`
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function summation(param){
+  for(let i = 0; i <= param; i++){
+    return param++;
+  }
+};
+console.log(summation(4));
